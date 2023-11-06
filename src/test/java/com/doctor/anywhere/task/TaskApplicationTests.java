660d7492c88b2199d089485dd48d133e37693b6f
Long Quang <tran50787@gmail.com>
2023-11-06 16:31:01 +0700

    initalize project

---
+++ src/test/java/com/doctor/anywhere/task/TaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doctor.anywhere.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
